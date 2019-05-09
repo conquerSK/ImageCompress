db9ed3d32b2045ea8ee935aff58309adc1c8b852
happykai <823109795@qq.com>
2019-05-09 23:08:43 +0800

    Initial commit

---
+++ app/src/main/java/com/shenkai/imagecompress/BitmapUtils.java
@@ -0,0 +1,60 @@
+package com.shenkai.imagecompress;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+/**
+ * Created by shenkai on 2019/5/9.
+ * desc:
+ */
+public class BitmapUtils {
+    public static Bitmap getScaledBitmap(String pathName, int reqWidth, int reqHeight) {
+        // 首先设置 inJustDecodeBounds=true 来获取图片尺寸
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(pathName, options);
+
+        // 计算 inSampleSize 的值
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // 根据计算出的 inSampleSize 来解码图片生成Bitmap
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(pathName, options);
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
+        // 首先设置 inJustDecodeBounds=true 来获取图片尺寸
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // 计算 inSampleSize 的值
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // 根据计算出的 inSampleSize 来解码图片生成Bitmap
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // 原始图片的宽高
+        int height = options.outHeight;
+        int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            int halfHeight = height / 2;
+            int halfWidth = width / 2;
+
+            // 在保证解析出的bitmap宽高分别大于目标尺寸宽高的前提下，取可能的inSampleSize的最大值
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+}
